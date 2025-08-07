0dded3eaa4651cc82f4db92d0369ef7b83164298
Milad <milad@example.com>
2025-08-07 13:17:26 +0330

    adding application configuration

--- authz/authz.py
+++ authz/authz.py
@@ -1,8 +1,10 @@
 # authz/authz.py
 
 from flask import Flask
-
+from authz.config import config
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+    print(app.config)
     return app
 
