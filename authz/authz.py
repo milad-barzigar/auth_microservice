fd9091cbacf9a583bb9e80c0c2459b3ccd099c62
Milad <milad@example.com>
2025-08-08 18:11:16 +0330

    initializing sqlalchemy structure

--- authz/authz.py
+++ authz/authz.py
@@ -2,17 +2,27 @@
 
 from flask import Flask, Blueprint
 from flask_restful import Api
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from authz.config import config
 
-apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
-paiv1 = Api(apiv1_bp)
+db = SQLAlchemy()
+mg = Migrate()
 
-from authz import resource 
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
+apiv1 = Api(apiv1_bp)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+
+    db.init_app(app)
+    mg.init_app(app, db)
+
+    
+    from authz import resource  
+
     app.register_blueprint(apiv1_bp)
     return app
+
 
--- authz/config/config.py
+++ authz/config/config.py
@@ -1,8 +1,23 @@
 from os import environ
 
 class config:
+
+##################### Application config #####################
+
     ENV = environ.get("TECHLAND_AUTHZ_ENV", "production")
+    
     DEBUG = bool(int(environ.get("TECHLAND_AUTHZ_DEBUG", "0")))
+    
     TESTING = bool(int(environ.get("TECHLAND_AUTHZ_TESTING", "0")))
+    
     SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
     
+    ##################### Database config #####################
+    
+    SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI", None)
+    
+    SQLALCHEMY_ECHO = DEBUG
+    
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
