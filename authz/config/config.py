2e7631dd2e5f069591228d9ba209e58e03ecdbb8
Milad <milad@example.com>
2025-08-08 21:00:35 +0330

    updateapplication configs

--- authz/config/config.py
+++ authz/config/config.py
@@ -12,6 +12,7 @@ class config:
     
     SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
     
+    TIMEZONE = environ.get("TECHLAND_AUTHZ_TIMEZONE", "Asia/Tehran")
     ##################### Database config #####################
     
     SQLALCHEMY_DATABASE_URI = environ.get("TECHLAND_AUTHZ_DATABASE_URI", None)
@@ -21,3 +22,11 @@ class config:
     SQLALCHEMY_RECORD_QUERIES = DEBUG
     
     SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
+     ##################### User config ##########################
+     
+     USER_DEFAULT_ROLE = environ.get("TECLAND_AUTHZ-USER-DEFAULT_ROLE", "member")
+     
+     USER_DEFAULT_EXPIRY_TIME = int(environ.get("TECLAND_AUTHZ-USER-DEFAULT_EXPIRY_TIME", "365"))
+     
+     USER_DEFAULT_STATUS = int(environ.get("TECLAND_AUTHZ-USER-DEFAULT_STATUS", "3"))
+   
